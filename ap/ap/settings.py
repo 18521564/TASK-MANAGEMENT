5aec908be8725769b02299c3d75a728474075970
18521564 <1852564@gm.uit.edu.vn>
2024-10-31 10:54:07 +0700

    add Procfile

--- ap/ap/settings.py
+++ ap/ap/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^q1@-8k%@t%y4#6ll^86t=2%y+7b)usb(a)19e8&6#hza^3%ig"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -78,6 +80,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+        'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
     }
 }
 
